5780d801b375be09e97818cc776c1b7e00df88cf
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2024-10-07 16:22:50 +0200

    Added some tests for python

---
+++ python/tests/test_reserve.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from remotebmi.reserve import reserve_values
+from tests.fake_models import Float32Model
+
+
+def test_reserve_values():
+    model = Float32Model()
+    result = reserve_values(model, "somevar")
+    assert result.dtype == np.float32
+    assert result.size == 3
