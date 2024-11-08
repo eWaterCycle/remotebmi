7fe7ac646008b6ff8b17541c19db841b7ee2ff22
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2024-11-08 09:11:51 +0100

    Format

--- python/tests/test_client.py
+++ python/tests/test_client.py
@@ -3,23 +3,30 @@ import pytest
 from remotebmi.client.client import RemoteBmiClient
 
 
-@pytest.mark.parametrize("url", [
-    "invalid_url",
-    "http://",
-    "ws://somehost",
-    "ftp://invalid",
-    "12345",
-    "",
-    None,
-])
+@pytest.mark.parametrize(
+    "url",
+    [
+        "invalid_url",
+        "http://",
+        "ws://somehost",
+        "ftp://invalid",
+        "12345",
+        "",
+        None,
+    ],
+)
 def test_remote_bmi_client_init_invalid_url(url: str):
     with pytest.raises(ValueError):
         RemoteBmiClient(url)
 
-@pytest.mark.parametrize("url", [
-    "http://somehost",
-    "https://somehost",
-])
+
+@pytest.mark.parametrize(
+    "url",
+    [
+        "http://somehost",
+        "https://somehost",
+    ],
+)
 def test_remote_bmi_client_init_valid_url(url: str):
     client = RemoteBmiClient(url)
     assert client.client.base_url == url
